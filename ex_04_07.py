02317b21b04f99e6ca00d0a57f180b5297d350a1
Ann <ananyu0606@gmail.com>
2017-10-20 23:15:36 +0800

    Add files via upload

---
+++ ex_04_07.py
@@ -0,0 +1,23 @@
+score = input("Enter Score: ")
+# If entering score is not numeric or is not a valid number, end the program
+try:
+ score = float(score)
+except:
+ print('Bad score')
+ quit()
+#grading with the following table
+def computegrade(score):
+    if score <= 0 or score >= 1.0:
+     print('Bad score')
+    elif score >= 0.9:
+     print('A')
+    elif score >= 0.8:
+     print('B')
+    elif score >= 0.7:
+     print('C')
+    elif score >= 0.6:
+     print('D')
+    else:
+     print('F')
+
+computegrade(score)
